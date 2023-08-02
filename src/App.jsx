86e0b536f5ecfccb9903452e502d9ed46a26378c
Atoche Pascual <jmatochepascual@gmail.com>
2023-08-02 12:43:25 -1000

    [FEATURE] buttom component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Buttom from "./components/Buttom"
 import Header from "./components/Header"
 import Rango from "./components/Rango"
 
@@ -6,6 +7,11 @@ function App() {
   return (
     <div className="max-w-lg my-20 mx-auto p-10 rounded-lg shadow-lg bg-white">
       <Header />
+
+      <div className="my-5 flex justify-between">
+        <Buttom />
+        <Buttom />
+      </div>
 
       <Rango />
     </div>
---
+++ src/components/Buttom.jsx
@@ -0,0 +1,9 @@
+const Buttom = () => {
+  return (
+    <button className="w-10 h-10 flex items-center justify-center rounded-full font-bold text-2xl text-center text-white bg-lime-500 hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500">
+      +
+    </button>
+  )
+}
+
+export default Buttom
