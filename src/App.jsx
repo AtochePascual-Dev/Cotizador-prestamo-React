68295f912eba4197cb8e767f9ae3768bec0c4194
Atoche Pascual <jmatochepascual@gmail.com>
2023-08-02 12:29:08 -1000

    [FEATURE] rango component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Header from "./components/Header"
+import Rango from "./components/Rango"
 
 function App() {
 
   return (
     <div className="max-w-lg my-20 mx-auto p-10 rounded-lg shadow-lg bg-white">
       <Header />
+
+      <Rango />
     </div>
   )
 }
---
+++ src/components/Rango.jsx
@@ -0,0 +1,9 @@
+const Rango = () => {
+  return (
+    <input
+      type="range"
+      className="w-full h-6 cursor-pointer accent-lime-500 hover:accent-lime-600" />
+  )
+}
+
+export default Rango
