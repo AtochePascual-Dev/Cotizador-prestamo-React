4e28e34a069f8916c97ee1a388cdb23996e06d41
Atoche Pascual <jmatochepascual@gmail.com>
2023-08-02 14:20:15 -1000

    [FEATURE] decrementar cantidad

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import Buttom from "./components/Buttom"
 import Header from "./components/Header"
 import Meses from "./components/Meses"
@@ -5,19 +6,34 @@ import Rango from "./components/Rango"
 import Resultado from "./components/Resultado"
 
 function App() {
+  const [cantidad, setCantidad] = useState(10000);
+
+  const handleClickDecremento = () => {
+    const valor = cantidad - 100;
+    if (valor < 0) return;
+    setCantidad(valor)
+  };
 
   return (
     <div className="max-w-lg my-20 mx-auto p-10 rounded-lg shadow-lg bg-white">
       <Header />
 
       <div className="mb-5 flex justify-between">
-        <Buttom />
-        <Buttom />
+        <Buttom
+          operador="-"
+          fn={handleClickDecremento}
+        />
+        <Buttom
+          operador="+"
+        />
       </div>
 
-      <Rango />
+      <Rango
+        cantidad={cantidad}
+        setCantidad={setCantidad}
+      />
 
-      <p className="mb-5 text-6xl text-center font-extrabold text-indigo-600">1000</p>
+      <p className="mb-5 text-6xl text-center font-extrabold text-indigo-600">{cantidad}</p>
 
       <h2 className="text-2xl font-extrabold text-gray-600 text-center">
         Elige un <span className="text-indigo-600">plazo</span> a pagar
--- src/components/Rango.jsx
+++ src/components/Rango.jsx
@@ -1,8 +1,15 @@
-const Rango = () => {
+const Rango = ({ cantidad, setCantidad }) => {
+
+
   return (
     <input
       type="range"
-      className="w-full h-6 mb-5 cursor-pointer accent-lime-500 hover:accent-lime-600" />
+      className="w-full h-6 mb-5 cursor-pointer accent-lime-500 hover:accent-lime-600"
+      min="0"
+      max="20000"
+      step="100"
+      value={cantidad}
+      onChange={(e) => setCantidad(Number(e.target.value))} />
   )
 }
 
